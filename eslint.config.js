da99605909d73c0a8d7355b2c35e291ddab0b4ff
Björn Reetz <git@bjoern-reetz.de>
2025-10-22 14:41:15 +0200

    tweak eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,23 @@
-import prettier from 'eslint-config-prettier/flat';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import vue from 'eslint-plugin-vue';
 
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import {
+    defineConfigWithVueTs,
+    vueTsConfigs,
+} from '@vue/eslint-config-typescript';
 
 export default defineConfigWithVueTs(
     vue.configs['flat/essential'],
     vueTsConfigs.recommended,
     {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'resources/js/components/ui/*'],
+        ignores: [
+            'vendor',
+            'node_modules',
+            'public',
+            'bootstrap/ssr',
+            'tailwind.config.js',
+            'resources/js/components/ui/*',
+        ],
     },
     {
         rules: {
@@ -15,5 +25,5 @@ export default defineConfigWithVueTs(
             '@typescript-eslint/no-explicit-any': 'off',
         },
     },
-    prettier,
+    eslintConfigPrettier,
 );
